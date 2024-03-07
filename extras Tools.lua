ad450906be323ec293d1e2a4cb2f8006794263c5
gstefani <137325085+gstefani@users.noreply.github.com>
2024-03-07 00:25:19 -0300

    Update extras Tools.lua

--- extras Tools.lua
+++ extras Tools.lua
@@ -9,7 +9,26 @@ onKeyPress(function(keys)
     end
 end)
 
-macro(100, "Hold Target", function()
+local macroName = ".:: Use With Delay ::."
+local items = {7439} -- always inside {}
+local wait = 1 -- minutes
+setDefaultTab("tools")
+
+macro(2000,macroName,function()
+  local time = 0
+  for i=1,#items do
+    local id = items[i]
+    if findItem(id) then
+      schedule(time,function()
+        g_game.use(findItem(id))
+      end)
+      time = time + 250
+    end
+  end
+  delay((wait*2*1000))
+end)
+
+macro(100, ".:: Hold Target ::.", function()
     -- if attacking then save it as target, but check pos z in case of marking by mistake on other floor
     if target() and target():getPosition().z == posz() and not target():isNpc() then
         targetID = target():getId()
@@ -30,7 +49,7 @@ macro(100, "Hold Target", function()
 end) 
 
 -- Hotkeys parar Target e Cave
-hotkey("Insert", "Toggle Target", function()
+hotkey("Insert", ".:: Toggle Target ::.", function()
 if TargetBot.isOff() then
 TargetBot.setOn()
 warn("TargetBot " .. (TargetBot.isOn() and 'On' or 'Off'))
@@ -41,19 +60,19 @@ warn("TargetBot " .. (TargetBot.isOn() and 'On' or 'Off'))
 return end
 end) 
 
-hotkey("Pageup", "ReturnCave", function()
+hotkey("Pageup", ".:: ReturnCave ::.", function()
 CaveBot.setOn()
 warn("CaveBot ON")
 end) 
 
-hotkey("Pagedown", "PauseCave", function()
+hotkey("Pagedown", ".:: PauseCave ::.", function()
 CaveBot.setOff()
 warn("CaveBot OFF")
 end)
 
 -- Mystic defense e mystic kai
 local castBelowHp = 70
-macro(50, "Mystic Defense",  function()
+macro(50, ".:: Mystic Defense ::.",  function()
   if (hppercent() <= castBelowHp and not hasManaShield()) then
     say('Mystic Defense')
   end
@@ -63,7 +82,7 @@ macro(50, "Mystic Defense",  function()
 end)
 
 -- Swapar weapon de acordo com a distancia do alvo
-macro(10, "Dist/Melee",function()
+macro(10, ".:: Dist/Melee ::.",function()
   if not g_game.isAttacking() then return end
   target = g_game.getAttackingCreature()
     if getDistanceBetween(player:getPosition(), target:getPosition()) > 1 then
@@ -82,7 +101,7 @@ Turn = {}
 
 Turn.maxDistance = {x = 7, y = 7}
 Turn.minDistance = 1
-Turn.macro = macro(1, 'Turn by Ryan', function()
+Turn.macro = macro(1, '.:: Turn by Ryan ::.', function()
     local target = g_game.getAttackingCreature()
     if target then
         local targetPos = target:getPosition()
@@ -122,7 +141,7 @@ if player:getBlessings() == 0 then
 end
 
 -- Abrir PMs em nova janela
-local privateTabs = addSwitch("openPMTabs", "PM Tabs", function(widget) widget:setOn(not widget:isOn()) storage.OpenPrivateTabs = widget:isOn() end, parent)
+local privateTabs = addSwitch("openPMTabs", ".:: PM Tabs ::.", function(widget) widget:setOn(not widget:isOn()) storage.OpenPrivateTabs = widget:isOn() end, parent)
 privateTabs:setOn(storage.OpenPrivateTabs)
 
 onTalk(function(name, level, mode, text, channelId, pos)
@@ -139,23 +158,48 @@ onTalk(function(name, level, mode, text, channelId, pos)
 end)
 
 -- Bugmap pelo mouse
-macro(50, "FollowMouseBugMap", "F1", function(m)
+macro(1, ".:: Bug Map - Mouse ::.", function(m)
     --Made By VivoDibra#1182 
     local tile = getTileUnderCursor()
     if not tile then return end
-    if tile:getTopThing() == g_game.getLocalPlayer() then
-        return m.setOff()
-    end
+    --if tile:getTopThing() == g_game.getLocalPlayer() then
+        --return m.setOff()
+    --end
+	if g_mouse.isPressed(7) then
     g_game.use(tile:getTopUseThing())
+	end
 end)
 
--- Magia em area Safe
-macro(200, "Magia Area SAFE", function()
- if g_game:isAttacking() and getPlayers(10, false) == 0 then
-  say(storage.customSpell1)
-  delay(50)
+-- Bugmap WASD
+local function checkPos(x, y)
+ xyz = g_game.getLocalPlayer():getPosition()
+ xyz.x = xyz.x + x
+ xyz.y = xyz.y + y
+ tile = g_map.getTile(xyz)
+ if tile then
+  return g_game.use(tile:getTopUseThing())  
+ else
+  return false
  end
-end)
-  addTextEdit("spell1", storage.customSpell1 or "", function(widget, spell1) 
-  storage.customSpell1 = spell1
-  end)
+end
+
+consoleModule = modules.game_console
+macro(1, '.:: Bug Map - WASD ::.', "F1", function() 
+ if modules.corelib.g_keyboard.isKeyPressed('w') and not consoleModule:isChatEnabled() then
+  checkPos(0, -5)
+ elseif modules.corelib.g_keyboard.isKeyPressed('e') and not consoleModule:isChatEnabled() then
+  checkPos(3, -3)
+ elseif modules.corelib.g_keyboard.isKeyPressed('d') and not consoleModule:isChatEnabled() then
+  checkPos(5, 0)
+ elseif modules.corelib.g_keyboard.isKeyPressed('c') and not consoleModule:isChatEnabled() then
+  checkPos(3, 3)
+ elseif modules.corelib.g_keyboard.isKeyPressed('s') and not consoleModule:isChatEnabled() then
+  checkPos(0, 5)
+ elseif modules.corelib.g_keyboard.isKeyPressed('z') and not consoleModule:isChatEnabled() then
+  checkPos(-3, 3)
+ elseif modules.corelib.g_keyboard.isKeyPressed('a') and not consoleModule:isChatEnabled() then
+  checkPos(-5, 0)
+ elseif modules.corelib.g_keyboard.isKeyPressed('q') and not consoleModule:isChatEnabled() then
+  checkPos(-3, -3)
+ end
+end) 
