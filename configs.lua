a92ddc576ec4a957f0f4e238a1a74766590554b6
Gustavo Stefani <137325085+gstefani@users.noreply.github.com>
2025-04-21 16:16:34 -0300

    Update configs.lua

--- configs.lua
+++ configs.lua
@@ -1,6 +1,30 @@
 --setDefaultTab("Others")
 UI.Separator()
 
+-- Mana treiner hunt
+local config = {
+    regen_mana_by_spell = false, -- se o teu regen de mana for por spell, deixe true, se n, false
+    regen_mana_spell = '', -- spell p regenerar mana
+    regen_mana_by_item = false, -- se o teu regen de mana for por item, deixe true, se n, false
+    regen_mana_id_item = 11863, -- item p regenerar mana
+    percent_train_ml = 90, -- porcentagem que irá intercalar entre regenerar e treinar, < regen > train
+    spell_train = '', -- spell de treino
+}
+
+macro(100, "Mana Trainer Hunt", function()
+    if manapercent() <= config.percent_train_ml then
+        if config.regen_mana_by_item then
+            useWith(config.regen_mana_id_item, player)
+        elseif config.regen_mana_by_spell then
+            say(config.regen_mana_spell)
+        end
+    else
+        if not g_game.isAttacking() then
+            say(config.spell_train)
+        end
+    end
+end)
+
 -- ICON Ligar e Desligar CaveBot/TargetBot
 local cIcon = addIcon("cI",{text="Cave\nBot",switchable=false,moveable=true}, function()
   if CaveBot.isOff() then 
