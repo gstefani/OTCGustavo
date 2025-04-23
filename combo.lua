0cda753113adfe1af1af69e36745b6035d765eac
Gustavo Stefani <137325085+gstefani@users.noreply.github.com>
2025-04-22 21:56:43 -0300

    Update combo.lua

--- combo.lua
+++ combo.lua
@@ -14,7 +14,7 @@ storage.uComboPlayers = storage.uComboPlayers or {}
 storage.comboMacroActive = storage.comboMacroActive or false
 
 -- Macro principal, roda só se o controle estiver ativo
-comboMacro = macro(100, "Combo", function()
+comboMacro = macro(100, function()
   if not storage.comboMacroActive then return end
   local target = g_game.getAttackingCreature()
   if not target then return end
