eca632e8c2ecc75685242d24693ebbbc9a025d3f
Gustavo Stefani <137325085+gstefani@users.noreply.github.com>
2025-04-23 23:56:20 -0300

    Update fpsButton.lua

--- fpsButton.lua
+++ fpsButton.lua
@@ -2,9 +2,65 @@ UI.Separator()
 local lbT = UI.Label('Reduzir FPS');
 lbT:setColor('orange');
 
-UI.Button("10 fps", function()
- modules.client_options.setOption("backgroundFrameRate", 10)
-end)
-UI.Button("150 fps", function()
- modules.client_options.setOption("backgroundFrameRate", 150)
+-- UI.Button("10 fps", function()
+--  modules.client_options.setOption("backgroundFrameRate", 10)
+-- end)
+-- UI.Button("150 fps", function()
+--  modules.client_options.setOption("backgroundFrameRate", 150)
+-- end)
+
+------------------------------------------------------
+local secondsToIdle = 5
+local activeFPS =  60
+---------------------------------------------------------
+
+local afkFPS = 0
+function botPrintMessage(message)
+  modules.game_textmessage.displayGameMessage(message)
+end
+
+botPrintMessage("[Idle-Mode] made by: VivoDibra#1182")
+
+local function isSameMousePos(p1,p2)
+  return p1.x == p2.x and p1.y == p2.y
+end
+
+local function setAfk()
+  modules.client_options.setOption("backgroundFrameRate", afkFPS)
+  modules.game_interface.gameMapPanel:hide()
+end
+
+local function setActive()
+  modules.client_options.setOption("backgroundFrameRate", activeFPS)
+  modules.game_interface.gameMapPanel:show()
+end
+
+local lastMousePos = nil
+local finalMousePos = nil
+local idleCount = 0
+local maxIdle = secondsToIdle * 4
+macro(250, "Idle Mode", function()
+  local currentMousePos = g_window.getMousePosition()
+
+  if finalMousePos then
+    if isSameMousePos(finalMousePos,currentMousePos) then return end
+    botPrintMessage("(Idle Mode) Active!")
+    setActive()
+    finalMousePos = nil
+  end
+
+  if lastMousePos and isSameMousePos(lastMousePos,currentMousePos) then
+    idleCount = idleCount + 1
+  else
+    lastMousePos = currentMousePos
+    idleCount = 0
+  end
+
+  if idleCount == maxIdle then
+    botPrintMessage("(Idle Mode) AFK!")
+    setAfk()
+    finalMousePos = currentMousePos
+    idleCount = 0
+  end
+
 end)
