a9208d8f4b1b19906ede5f94f200d28370ae050f
Gustavo Stefani <gustavo.stefani2013@gmail.com>
2025-05-05 15:43:44 -0300

    ajuste separator

--- fpsButton.lua
+++ fpsButton.lua
@@ -1,14 +1,6 @@
 setDefaultTab("News")
-UI.Separator()
 local lbT = UI.Label('Reduzir FPS');
 lbT:setColor('orange');
-
--- UI.Button("10 fps", function()
---  modules.client_options.setOption("backgroundFrameRate", 10)
--- end)
--- UI.Button("150 fps", function()
---  modules.client_options.setOption("backgroundFrameRate", 150)
--- end)
 
 ------------------------------------------------------
 local secondsToIdle = 120
